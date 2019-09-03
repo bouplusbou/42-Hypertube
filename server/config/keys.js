3ef70c57967c6c1581a1fa0d6308a89cff9b951f
Baptiste Boucher <baptiste.boucher@gmail.com>
2019-09-03 13:39:54 +0200

    Delete keys.js

--- server/config/keys.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    mongoURI: "mongodb+srv://admin:mangousteDB@hypertube-evyoa.mongodb.net/test?retryWrites=true&w=majority",
-    secretOrKey: "secret",
-    FORTYTWO_APP_ID: "27458520bc43ea0e11f51461c31a3fb3287f2a0bfb4a2933c775d18b142d0ff1",
-    FORTYTWO_APP_SECRET: "3f669fb842454f86e59fad0b236dcbc4591e1267b93f9982b8f30472e11a46c0",
-};
