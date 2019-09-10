00266fe7012f8b3ae2c42a1a8e38f50bfbb9647d
Baptiste BOUCHER <bboucher@e1r1p9.42.fr>
2019-09-10 19:41:04 +0200

    add authToken protection in header

--- client/src/appAuthenticated/Header/Header.js
+++ client/src/appAuthenticated/Header/Header.js
@@ -94,7 +94,7 @@ export default function HeaderComp() {
                 console.log(err);
             }
         }
-        fetchData();
+        if (authToken) fetchData();
         return () => isSubscribed = false;
     })
 
