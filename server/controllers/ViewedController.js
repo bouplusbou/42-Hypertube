72520802ecabb0a105c797221020f1a876b43ff6
Guillaume LAVIGNOTTE <36131064+glavigno@users.noreply.github.com>
2019-09-17 11:49:26 +0200

    viewed continued, started cron

--- server/controllers/ViewedController.js
+++ server/controllers/ViewedController.js
@@ -2,16 +2,31 @@ const UserModel = require('../models/UserModel');
 const MovieModel = require('../models/MovieModel');
 const jwt = require('jsonwebtoken');
 const keys = require('../config/keys');
+var CronJob = require('cron').CronJob;
 
 const setViewed = async (req, res) => {
     try {
         const { authToken } = req.query;
         const decoded = await jwt.verify(authToken, keys.JWT_SECRET);
         const _id = decoded.mongoId;
-        const query = { "imdbId" : req.body.imdbId};
+        const imdbId = req.body.imdbId;
+        const query = { "imdbId" : imdbId };
         const date = Date.now()
         const update = { lastViewed: date }
         await MovieModel.updateOne(query, update)
+        await UserModel.updateOne(
+            { "_id" : _id},
+            { $push: {
+                viewedList: {
+                    $each: [imdbId],
+                    $position : 0
+                }}
+            })
+            let now = new Date();
+            now.setSeconds(now.getSeconds() + 10);
+        new CronJob(now, () => {
+            console.log('You will see this message every secondHOOOOOOOOOOOOOO');
+          });
     } catch(err) {
         console.log(err);
         res.status(500).json({ error: err});
