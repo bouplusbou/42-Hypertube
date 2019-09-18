44e394e473d7edf575572ec8d6fe6adb6e8a12aa
Baptiste BOUCHER <bboucher@e1r1p10.42.fr>
2019-09-18 12:11:45 +0200

    gitignore fix

--- server/node_modules/yifysubtitles/test/api-alive.js
+++
@@ -1,8 +0,0 @@
-import test from 'ava';
-import got from 'got';
-
-test(`http://yifysubtitles.com should be alive`, async t => {
-	const res = await got(`http://yifysubtitles.com`);
-
-	t.is(res.statusCode, 200);
-});
